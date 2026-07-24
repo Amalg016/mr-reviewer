0977b3c58ed3bf52aff94b3c1d4fdebc98f86ed9
Amalg016 <amalgjoseph1@gmail.com>
2026-07-25 01:55:29 +0530

    Added logic to fetch all the existing MRs made on current branch

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
 use std::process::Command;
 
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct MergeRequest {
+    iid: u64,
+    title: String,
+    web_url: String,
+}
+
+async fn find_mr(
+    project: &str,
+    branch: &str,
+) -> Result<Vec<MergeRequest>, reqwest::Error> {
+
+    let project = urlencoding::encode(project);
+
+    let url = format!(
+        "https://gitlab.com/api/v4/projects/{}/merge_requests?source_branch={}",
+        project,
+        branch
+    );
+
+    let response = reqwest::get(url).await?.json::<Vec<MergeRequest>>().await?;
+
+    Ok(response)
+}
+
 fn get_current_branch() -> Result<String, String> {
     let output = Command::new("git")
         .args(["branch", "--show-current"])
@@ -22,7 +49,8 @@ fn get_current_branch() -> Result<String, String> {
     Ok(branch)
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let project = "gitlab-org/gitlab";
     let branch = match get_current_branch() {
         Ok(branch) => branch,
@@ -32,6 +60,21 @@ fn main() {
         }
     };
 
-println!("Branch: {}", branch);
+    println!("Branch: {}", branch);
 
+    match find_mr(project, &branch).await {
+        Ok(mrs) => {
+             if mrs.is_empty() {
+                println!("No merge request found for {}", branch);
+             } else {
+                for mr in mrs {
+                    println!("#{} :- {}", mr.iid, mr.title);
+                    println!("url: {}", mr.web_url);
+                }
+            }
+        }
+        Err(err) => {
+            eprintln!("Failed to fetch MR: {}", err);
+        }
+    }
 }
